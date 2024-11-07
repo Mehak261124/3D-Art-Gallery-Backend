5a16803b526278453f03bbcb3ad44f948f8c5313
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-11-08 02:56:38 +0530

    cors updated

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://3d-art-gallery-taupe.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
